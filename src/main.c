b7e2ce9219528e22cae5e9e82c5d0ee356c98631
nahuelrudisi <nahuelrudisi@hotmail.com>
2016-09-16 10:27:29 -0300

    Laboratorio 2: 0.1

--- src/main.c
+++ src/main.c
@@ -2,8 +2,11 @@
 #include "bsp/bsp.h"
 #include "lib_draw.h"
 #include "imagenes/fondo.h"
+#include "imagenes/fondo4.h"
 #include "imagenes/nave0.h"
 #include "imagenes/nave1.h"
+#include "imagenes/pelota.h"
+#include "imagenes/minion.h"
 
 int main(void) {
 	uint8_t posx = 142, posy = 100;
@@ -12,13 +15,19 @@ int main(void) {
 	bsp_init();
 
 	while (1) {
-		VGA_DrawImage(&fondo, 0, 0);
+		VGA_DrawImage(&fondo4, 0, 0);
+		posx = 142, posy = 100;
 		if (nave++ % 8 > 3) {
-			VGA_DrawImageAlpha(&nave1, posx, posy, 0x1C);
+			VGA_DrawImageAlpha(&pelota, posx, posy, 0x01);
 		} else {
-			VGA_DrawImageAlpha(&nave0, posx, posy, 0x1C);
+			VGA_DrawImageAlpha(&pelota, posx, posy, 0x01);
 		}
-
+		posx = 125, posy = 50;
+		if (nave++ % 8 > 3) {
+			VGA_DrawImageAlpha(&minion, posx, posy, 0x01);
+		} else {
+			VGA_DrawImageAlpha(&minion, posx, posy, 0x01);
+		}
 		bsp_draw();
 	}
 }
